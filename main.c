164d848cfd98db0820a2bdc8820d33bc0338d2d3
juliobom <juliocesarbom25@outlook.com>
2026-05-08 16:12:03 -0300

    adicionado novas funcoes em main.c e correcoes com ajuste de melhorias, nova pasta criada (output).

--- main.c
+++ main.c
@@ -1,9 +1,94 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <conio.h>
 
+
+
+typedef struct{
+        char nome[50];
+        int data_validade,dia,mes,ano;
+        float preco;
+
+}produto;
+
+typedef struct{
+    int inserir,listar,remover,prog,opcao;
+}condicao;
+
+
+void cadastrar_produto(produto *p,int tam, condicao *flag){
+    int dia,mes,ano,count=0;
+    flag->inserir = 1;
+    while(flag->inserir != 0){
+        int qtd;
+        printf("Quantos produtos deseja cadastrar?\n");
+        scanf("%d", &qtd);
+        system("cls");
+        for(int i=0;i<qtd;i++){
+            printf("Digite o nome do produto: ");
+            scanf("%s", p[count].nome);
+            printf("Digite a data de validade (formato dia / mes / ano): ");
+            scanf("%d / %d / %d", &dia, &mes, &ano);
+            printf("Digite o preço do produto: ");
+            scanf("%f", &p[count].preco);
+            p[count].dia = dia;
+            p[count].mes = mes;
+            p[count].ano = ano;
+            printf("%d/%d/%d\n", p[count].dia, p[count].mes, p[count].ano);
+            printf("Produto cadastrado com sucesso!\n");
+            sleep(1);
+            system("cls");
+            count ++;//----> futuramente vou retornar um valor para funcao de listagem ou (criar uma variavel dinamicamente) para saber quantos produtos foram cadastrados no total.
+        }
+        
+        printf("Deseja cadastrar mais produto(s)? (1 - Sim, 0 - Nao): ");
+        scanf("%d", &flag->inserir);
+    }
+}
+
+void listagem(produto *p, int tam, condicao *flag){
+
+    for(int i=0;i<tam;i++){
+
+        printf("[%d] [ produto: %s | data de validade: %d/%d/%d | preco: %.2f ]\n", i+1, p[i].nome, p[i].dia, p[i].mes, p[i].ano, p[i].preco);
+    }
+
+
+
+}
 
 int main(){
-    printf("Hello, World!\n");//e assim vai commitando
-    return 0;   
+
+    produto a[10];
+    condicao flag;
+    flag.prog = 1;
+    while(flag.prog != 0){
+
+        printf("\n1 - cadastrar um produto\n\n2 - listar os produtos\n\n3 - remover um produto\n\n0 - sair\n\nDigite a opcao desejada:  ");
+        scanf("%d", &flag.prog);
+        system("cls");
+        switch (flag.prog){
+            case 1:
+                printf("Cadastro de produto\n\n");
+                sleep(1);
+                cadastrar_produto(a, 10, &flag);
+                break;
+            case 2:
+                listagem(a, 10, &flag);
+                break;
+            case 3:
+                //vai remover um produto cadastrado
+                break;
+            case 0:
+                printf("Saindo do programa...\n");
+                sleep(2);
+                break;
+        }
+    }
+      
+    return 0;
 }
 
 
