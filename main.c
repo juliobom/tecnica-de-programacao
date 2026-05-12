441a4b95ca002a2c83508c28f79ddcc8512cd4ef
juliobom <juliocesarbom25@outlook.com>
2026-05-12 12:49:55 -0300

    8º commit - criado as  funcoes | ordeneNome & ordeneId | nova variavel (id) na struct produto, corrigido bug contagem da lista com count para *list

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
-#include <conio.h>
 
 
-//para margen de vencimento estebelecido dia presente em 05/06/2025, sem data for menor receberao um rotulo de ""produto vencido".
-
+//para margen de vencimento, estebelecido dia presente em 05/06/2025, se data for menor recebera um rotulo de ""produto vencido".
 
 typedef struct{
         char nome[50];
-        int data_validade,dia,mes,ano,venc;
+        int data_validade,dia,mes,ano,venc,id;
         float preco;
 
 }produto;
@@ -20,62 +19,117 @@ typedef struct{
 }condicao;
 
 
-void ordene(produto *p,int tam){
-
-
-    for(int i=0;i<tam;i++){
-    p[i].data_validade = (p[i].ano-2025)*365 + (p[i].mes-1)*30 + p[i].dia;
-
-
-
-    }
+void ordeneId(int *tam,produto *key2){
     
+    produto aux;
+    int i,j;
+    for(i=1;i<*tam;i++){
+        aux=key2[i];
+        for(j=i-1;j>=0&&key2[j].id>aux.id;j--){
+
+            key2[j+1]=key2[j];
+
+        }
+        key2[j+1] = aux;
+    }
 }
 
-void cadastrar_produto(produto *p,int tam, condicao *flag){
-    int dia,mes,ano,count=0;
+
+void ordeneNome(int *tam,produto *key1){     
+    
+    produto aux;
+    int i,j;
+    for(i=1;i<*tam;i++){
+        
+        aux=key1[i];
+        for(j=i-1;j>=0&&strcmp(key1[j].nome,aux.nome)>0;j--){
+
+            key1[j+1]=key1[j];
+
+        }
+        key1[j+1] = aux;
+    }
+}
+
+void cadastrar_produto(produto *prod, condicao *flag, int *list,produto *key1,produto *key2){     
+    int dia,mes,ano;
     flag->inserir = 1;
     while(flag->inserir != 0){
+
         int qtd;
+
         printf("Quantos produtos deseja cadastrar?\n");
         scanf("%d", &qtd);
-        system("cls");
+            system("cls");
+
         for(int i=0;i<qtd;i++){
+
             printf("Digite o nome do produto: ");
-            scanf("%s", p[count].nome);
+            scanf("%s", prod[*list].nome);
+
             printf("Digite a data de validade (formato dia / mes / ano): ");
             scanf("%d / %d / %d", &dia, &mes, &ano);
                 if(ano<2025 || (ano==2025 && mes<6) || (ano==2025 && mes==6 && dia<5)){
-                    p[count].venc = 1;
+                    prod[*list].venc = 1;
+                }else{
+                    prod[*list].venc = 0;
                 }
                 
-            
             printf("Digite o preço do produto: ");
-            scanf("%f", &p[count].preco);
-            p[count].dia = dia;
-            p[count].mes = mes;
-            p[count].ano = ano;
-            printf("%d/%d/%d\n", p[count].dia, p[count].mes, p[count].ano);
+            scanf("%f", &prod[*list].preco);
+
+            prod[*list].dia = dia;
+            prod[*list].mes = mes;
+            prod[*list].ano = ano;
+            prod[*list].id=rand()%1000;
+
+            {
+                key1[*list] = prod[*list];
+                key2[*list] = prod[*list];
+            }  
+
             printf("Produto cadastrado com sucesso!\n");
-            sleep(1);
-            system("cls");
-            ordene(p,count);
-            count ++;//----> futuramente vou retornar um valor para funcao de listagem ou (criar uma variavel dinamicamente) para saber quantos produtos foram cadastrados no total.
+                sleep(1);
+                system("cls");
+            
+            *list += 1;
+            ordeneId( list, key2);
+            ordeneNome( list, key1);
             
         }
-
         printf("Deseja cadastrar mais produto(s)? (1 - Sim, 0 - Nao): ");
         scanf("%d", &flag->inserir);
     }
 }
 
-void listagem(produto *p, int tam, condicao *flag){     // flag vai ser usada para retornar ao menu principal quando nao querer mais listar os produtos.
+void listagem(produto *prod, condicao *flag, int *list, produto *key1, produto *key2){
 
-    for(int i=0;i<tam;i++){
+    
+    for(int i=0;i<*list;i++){
 
-        printf("[%d] [ produto: %s | data de validade: %d/%d/%d | preco: %.2f ]", i+1, p[i].nome, p[i].dia, p[i].mes, p[i].ano, p[i].preco);
-        if(p[i].venc==1){
-            printf(" (Produto vencido)\n");
+        printf("[%d] [id: %4.d] [ produto: %s | data de validade: %d/%d/%d | preco: %.2f ]", i+1, prod[i].id, prod[i].nome, prod[i].dia, prod[i].mes, prod[i].ano, prod[i].preco);
+        if(prod[i].venc==1){
+            printf(" (Produto vencido)");
+        }
+        printf("\n");
+    }
+
+    printf("listagem ordenada por nome\n");
+    for(int i=0;i<*list;i++){
+
+        printf("[%d] [id: %4.d] [ produto: %s | data de validade: %d/%d/%d | preco: %.2f ]", i+1, key1[i].id, key1[i].nome, key1[i].dia, key1[i].mes, key1[i].ano, key1[i].preco);
+        if(key1[i].venc==1){
+            printf(" (Produto vencido)");
+        }
+        printf("\n");
+    }
+
+    printf("listagem ordenada por id\n");
+    for(int i=0;i<*list;i++){
+
+        printf("[%d] [id: %4.d] [ produto: %s | data de validade: %d/%d/%d | preco: %.2f ]", i+1, key2[i].id, key2[i].nome, key2[i].dia, key2[i].mes, key2[i].ano, key2[i].preco);
+        if(key2[i].venc==1){
+            printf(" (Produto vencido)");
         }
         printf("\n");
     }
@@ -85,26 +139,27 @@ void listagem(produto *p, int tam, condicao *flag){     // flag vai ser usada pa
 }
 
 int main(){
-
-    int count;
-    produto a[10];
+    srand(time(NULL));
+    int count=0;
+    produto prod[100], key1[100], key2[100];
+    int* list = malloc(sizeof(int));
+    *list = 0;
     condicao flag;
     flag.prog = 1;
     while(flag.prog != 0){
 
         printf("\n1 - cadastrar um produto\n\n2 - listar os produtos\n\n3 - remover um produto\n\n0 - sair\n\nDigite a opcao desejada:  ");
         scanf("%d", &flag.prog);
-        system("cls");
+            system("cls");
         switch (flag.prog){
             case 1:
                 printf("Cadastro de produto\n\n");
-                sleep(1);
-                system("cls");
-                count = 0;              
-                cadastrar_produto(a, 10, &flag); //vai receber um ponteiro para uma variavel limite para saber quantos produtos foram cadastrados na hora de listar-los.
+                    sleep(1);
+                    system("cls");              
+                cadastrar_produto(prod, &flag, list, key1, key2);                  //vai receber um ponteiro para uma variavel limite para saber quantos produtos foram cadastrados na hora de listar-los.
                 break;
             case 2:
-                listagem(a, 10, &flag);
+                listagem(prod, &flag, list, key1, key2);     //vai receber um ponteiro para uma variavel limite para saber quantos produtos foram cadastrados para listar-los.
                 break;
             case 3:
                 //vai remover um produto cadastrado
@@ -114,14 +169,14 @@ int main(){
                 while (count<1)
                 {
                     printf("Saindo do programa.");
-                    sleep(1);
-                    system("cls");
+                        sleep(1);
+                        system("cls");
                     printf("Saindo do programa..");
-                    sleep(1);
-                    system("cls");
+                        sleep(1);
+                        system("cls");
                     printf("Saindo do programa...");
-                    sleep(1);
-                    system("cls");
+                        sleep(1);
+                        system("cls");
                     count++;
                 }
                 break;
