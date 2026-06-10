68f63d3f0122e8aed882660f6b3633ae6b61d236
juliobom <juliocesarbom25@outlook.com>
2026-06-10 20:44:41 -0300

    chore: Binario(lendo)

--- Binario(Lendo).c
+++ Binario(Lendo).c
@@ -18,8 +18,8 @@ int main(){
     FILE * arq;
   
     arq = fopen ("Alunadoooo2.bin", "rb");
-    while(fread (a , sizeof(aluno), 1, arq)==1);
-    
+    while(fread (a , sizeof(aluno), 1, arq)==1);  //aqui vai gravar na struct aluno presente em --> "a", lendo de 1 em 1, com tamanha predefinido de 1 voltada.
+                              
     for(int i=0;i<1;i++){
         
         printf("%s\n%d\n%f\n",a[i].nome,a[i].mat,a[i].nota);
